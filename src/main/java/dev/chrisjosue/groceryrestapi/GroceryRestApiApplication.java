ba3c48e250f58999793fadf72fa6062f01438a7f
Chris <chrismartinezlara@outlook.es>
2023-04-20 14:34:59 -0600

    First Commit

---
+++ src/main/java/dev/chrisjosue/groceryrestapi/GroceryRestApiApplication.java
@@ -0,0 +1,13 @@
+package dev.chrisjosue.groceryrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/chrisjosue/groceryrestapi/GroceryRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chrisjosue.groceryrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
